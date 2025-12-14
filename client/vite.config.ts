b4a6dec636e20f2bd838fb106292fbdcb63b5fd9
Abhilash kusa <kusa.abhilash@gmail.com>
2025-12-14 00:25:47 -0500

    fix(pwa): update manifest to use SVG icons

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,14 +17,14 @@ export default defineConfig({
         background_color: '#0a110e',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-192x192.svg',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/svg+xml'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-512x512.svg',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/svg+xml'
           }
         ]
       }
